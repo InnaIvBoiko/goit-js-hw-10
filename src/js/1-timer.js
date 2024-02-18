6f570dbbfb9c5e6f2815449430eb38657473ee92
InnaIvBoiko <inna_boiko@libero.it>
2024-02-18 10:28:51 +0100

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,64 +6,56 @@ import "izitoast/dist/css/iziToast.min.css";
 
 import ErrorIcon from '../img/error.svg';
 
-const dataSelect = document.querySelector('input#datetime-picker');
+const dataSelectInput = document.querySelector('input#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
 
 startBtn.disabled = true;
 
-const options = {
+flatpickr('input#datetime-picker', { 
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const timeNow = Date.now();
+    const userSelectedDate = new Date(selectedDates).getTime();
+    const delta = userSelectedDate - timeNow;
+
+    if (delta > 0) {
+      startBtn.disabled = false;
+    } else {
+      startBtn.disabled = true;
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        messageColor: '#fff',
+        messageSize: '18',
+        backgroundColor: '#ef4040',
+        iconUrl: ErrorIcon,
+        position: 'topRight',
+        close: false,
+        timeout: 3000
+      });
+    };
   },
-};
-
-flatpickr('input#datetime-picker', { options });
-
-dataSelect.addEventListener('input', () => {
-  const timeNow = Date.now();
-  const userSelectedDate = new Date(dataSelect.value).getTime();
-  const delta = userSelectedDate - timeNow;
-
-  if (delta > 0) {
-    startBtn.disabled = false;
-  } else {
-    startBtn.disabled = true;
-    iziToast.show({
-      message: 'Please choose a date in the future',
-      messageColor: '#fff',
-      messageSize: '18',
-      backgroundColor: '#ef4040',
-      iconUrl: ErrorIcon,
-      position: 'topRight',
-      close: false,
-      timeout: 3000
-    });
-  };
-});
+ });
 
 startBtn.addEventListener('click', () => {
   const taimerId = setInterval((() => {
-    const timeNow = Date.now();
-    const userSelectedDate = new Date(dataSelect.value).getTime();
-    const delta = userSelectedDate - timeNow;
-    if (delta === 0) {
-      clearInterval(taimerId);
-      document.querySelector('span[data-days]').innerHTML = "00";
-      document.querySelector('span[data-hours]').innerHTML = "00";
-      document.querySelector('span[data-minutes]').innerHTML = "00";
-      document.querySelector('span[data-seconds]').innerHTML = "00";
-    } else {
-      convertMs(delta);
+  const timeNow = Date.now();
+  const userSelectedDate = new Date(dataSelectInput.value).getTime();
+  const delta = userSelectedDate - timeNow;
+    
+  if (delta < 0) {
+    clearInterval(taimerId);
+    dataSelectInput.disabled = false;
+  } else {
+    convertMs(delta);
     };
   }),
-    1000);
+  1000);
   
   startBtn.disabled = true;
-  dataSelect.disabled = true;
+  dataSelectInput.disabled = true;
 });
 
 function convertMs(ms) {
